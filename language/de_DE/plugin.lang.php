10fee9b6aafb8a2e9a652dbb4de5a8767c9b4927
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-12 16:29:03 +0000

    [prune_history] Update de_DE, thanks to : Eric
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@14924 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_AutoPrune_false'] = 'Deaktivieren (Standard)';
+$lang['PH_AutoPrune_title_d'] = 'Wenn Sie die automatische Bereinigung zu ermöglichen, wird es automatisch bei jedem User-Login auf der Galerie (einschließlich eines Administrators oder Webmasters) ausgelöst werden und basierend auf den Naturschutz-Einstellungen.<br><br>
+<b style=&quot;color: red;&quot;>Mit Vorsicht! Zurückgeschnitten Daten nicht wiederhergestellt werden!</b>';
+$lang['PH_AutoPrune_title'] = 'Automatische Beschneidung';
+$lang['PH_AutoPrune_true'] = 'Aktivieren';
+$lang['PH_AutoRange_title_d'] = 'Bitte konfigurieren Sie Ihre History <b>Erhaltung</b> Kriterium für die automatische Bereinigung. Mehrere Optionen sind möglich:<br><br>
+- Halten Sie x Tage<br>
+- Halten Sie x Monate<br>
+- Halten Sie x Jahren<br><br>
+Zum Beispiel: Wenn Sie eine historische Tiefe von 3 Monaten behalten möchten, können Sie bis drei Monate oder 91 Tage.<br><br>
+Hinweis: Die History Zusammenfassung wird nicht durch automatische Beschneiden betroffen.';
+$lang['PH_AutoRange_title'] = 'Halten die History bis zu:';
+$lang['PH_Bad_version_backup'] = 'Falsche Plugin-Version! Das Backup Sie wiederherstellen möchten ist veraltet oder mit einer älteren Version des Plugins erstellt worden.';
+$lang['PH_Dump_Download'] = 'Um die Wiederherstellung-Datei herunterzuladen, aktivieren Sie dieses Feld:';
+$lang['PH_Dump_NOK'] = 'Das Backup ist fehlgeschlagen!';
+$lang['PH_Dump_OK'] = 'Das Backup war erfolgreich!';
+$lang['PH_ManualPrune_title_d'] = 'Diese Funktion ermöglicht es Ihnen, History-Einträge auf der Grundlage einer Reihe von Daten zu löschen. Wählen Sie ein Startdatum und Enddatum der Behandlung und Lauf.<br><br>
+<b style=&quot;color: red;&quot;>Warnung: Die History Daten werden unwiederbringlich gelöscht!</b> Sie können Backup und Restore History-Tabelle, indem Sie die Funktionen im rechten Frame.<br><br>
+Hinweis: Die History Zusammenfassung wird nicht durch Beschneidung betroffen.';
+$lang['PH_ManualPrune_title'] = 'Manuelle Beschneidung';
+$lang['PH_No_Backup_File'] = 'Kein Backup-Datei zum Wiederherstellen gefunden!';
+$lang['PH_Prune_Done'] = 'Manuelle Bereinigung getan!';
+$lang['PH_Prune_Error'] = 'Fehler manuellen Bereinigung! Überprüfen Sie Ihre Datumsbereich.';
+$lang['PH_Prune'] = 'Führen Sie manuellen Bereinigung';
+$lang['PH_Restoration_NOK'] = 'Wiederherstellung fehlgeschlagen!';
+$lang['PH_Restoration_OK'] = 'Wiederherstellung erfolgreich!';
+$lang['PH_Restore_File'] = 'Wiederherstellen History';
+$lang['PH_Restore'] = 'Um ein Backup Ihrer History-Tabelle wiederherstellen.<br>Lediglich die gespeicherte Datei auf dem Server berücksichtigt wird.';
+$lang['PH_Save_Title_d'] = 'Denn Beschneidung ist endgültig und unumkehrbar, ist es ratsam, Ihre History-Tabelle vor Beginn der Beschneidung zu retten. Standardmäßig wird die Tabelle in einer Datei auf dem Server gespeichert und können von dort wiederhergestellt werden.<br><br>
+Aber es ist auch möglich, die Wiederherstellung-Datei herunterladen, um es aus Ihrer Galerie. In diesem Fall muss die Restauration über Ihre Datenbank-Management-Tool (in der Regel phpMyAdmin) durchgeführt werden.';
+$lang['PH_Save_Title'] = 'Sicherheits-Backup';
+$lang['PH_Save'] = 'Backup History';
+$lang['PH_Support'] = 'Die offizielle Unterstützung auf diesem Plugin ist nur auf diesen Piwigo Forenbeitrag:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=19418" onclick="window.open(this.href);return false;">Englisch-Forum - http://piwigo.org/forum/viewtopic.php?id=19418</a>';
+$lang['PH_save_config'] = 'Einstellungen gespeichert!';
+$lang['PH_submit'] = 'Einstellungen speichern';
+$lang['Start_Date'] = 'Startdatum';
+$lang['range']['1'] = 'Tag(e)';
+$lang['range']['2'] = 'Monat(e)';
+$lang['range']['3'] = 'Jahr(e)';
+?>

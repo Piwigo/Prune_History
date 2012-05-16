a5ffab90e40c91ad5414cd0c8ab0bb962539a6e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-16 23:01:17 +0000

    [prune_history] Update it_IT, thanks to : Gdvpixel
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@15001 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_AutoPrune_false'] = 'Disabilita (default)';
+$lang['PH_AutoPrune_title'] = 'Puliza automatica';
+$lang['PH_AutoPrune_true'] = 'Abilita';
+$lang['PH_AutoRange_title'] = 'Mantieni lo storico fino a:';
+$lang['PH_Bad_version_backup'] = 'Versione del plug-in errata! Il backup che stai tentando di ripristinare è troppo vecchio oppure è stato creato con una versione precedente del plug-in.';
+$lang['PH_Dump_Download'] = 'Per scaricare il file di backup, seleziona questa casella';
+$lang['PH_Dump_NOK'] = 'Il backup è fallito!';
+$lang['PH_Dump_OK'] = 'Il backup è stato concluso con successo!';
+$lang['PH_ManualPrune_title'] = 'Pulitura manuale';
+$lang['PH_No_Backup_File'] = 'Nessun file di backup trovato per il ripristino!';
+$lang['PH_Prune_Done'] = 'Pulizia manuale eseguita!';
+$lang['PH_Prune_Error'] = 'Errore durante la pulizia! Verifica il range di date.';
+$lang['PH_Prune'] = 'Esegui pulizia manuale';
+$lang['PH_Restoration_NOK'] = 'Ripristino fallito!';
+$lang['PH_Restoration_OK'] = 'Ripristino concluso con successo!';
+$lang['PH_Restore_File'] = 'Restore history';
+$lang['PH_Restore'] = 'Per ripristinare un backup della tabella history.<br>Solo i file salvati sul server sono inseriti nell\'account';
+$lang['PH_Save_Title'] = 'Backup di sicurezza';
+$lang['PH_Save'] = 'Backup history';
+$lang['PH_save_config'] = 'Impostazioni salvate!';
+$lang['PH_submit'] = 'Salva impostazioni';
+$lang['Start_Date'] = 'Data di inizio';
+$lang['range']['1'] = 'Giorni';
+$lang['range']['2'] = 'Mesi';
+$lang['range']['3'] = 'Anni';
+?>

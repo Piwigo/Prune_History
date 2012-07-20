25ddcc0406adc0c023ea684420ac1aade7b8916b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-20 19:55:07 +0000

    [prune_history] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@16873 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_AutoPrune_false'] = 'Atspējot (pēc noklusējuma)';
+$lang['PH_AutoPrune_title'] = 'Automātiskā Apgriešana';
+$lang['PH_AutoPrune_true'] = 'Iespējot';
+$lang['PH_AutoRange_title'] = 'glabāt vēsturi līdz:';
+$lang['PH_Bad_version_backup'] = 'Aplama spraudņa versija!Rezerves kopija, ko mēģināt atjaunot, ir novecojusi vai ir izveidota, izmantojot kādu no vecākām spraudņa versijām.';
+$lang['PH_Dump_Download'] = 'Lai lejupielādētu rezerves kopiju, veiciet atzīmi šajā lodziņā:';
+$lang['PH_Dump_NOK'] = 'Rezerves kopijas izveide neizdevās!';
+$lang['PH_Dump_OK'] = 'Rezerves kopijas izveide sekmīga!';
+$lang['PH_ManualPrune_title'] = 'Pielāgotā Apgriešana';
+$lang['PH_No_Backup_File'] = 'Nav atrasts rezerves kopijas fails, ko atjaunot! ';
+$lang['PH_Prune_Done'] = 'Pielāgotā apgriešana paveikta!';
+$lang['PH_Prune_Error'] = 'Kļūda veicot pielāgoto apgriešanu! Pārbaudiet pielietoto datu diapazonu.';
+$lang['PH_Prune'] = 'Veikt pielāgoto apgriešanu';
+$lang['PH_Restoration_NOK'] = 'Atjaunošana nesekmīga!';
+$lang['PH_Restoration_OK'] = 'Atjaunošana sekmīga!';
+$lang['PH_Restore_File'] = 'Atjaunot vēsturi';
+$lang['PH_Save_Title'] = 'Droša rezerves kopija ';
+$lang['PH_Save'] = 'Rezerves kopiju vēsture';
+$lang['PH_save_config'] = 'Iestatījumi saglabāti!';
+$lang['PH_submit'] = 'Saglabāt iestatījumus';
+$lang['Start_Date'] = 'Sākuma datums';
+$lang['range']['1'] = 'Diena(s)';
+$lang['range']['2'] = 'Mēnesis(ši)';
+$lang['range']['3'] = 'Gads(i)';
+?>

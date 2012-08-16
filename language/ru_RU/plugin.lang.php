c3fa23e1aa353c2d26d3a88156d7600842e8c87f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-16 06:34:46 +0000

    [prune_history] Update ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@17582 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_AutoPrune_false'] = 'Запретить (по умолчанию)';
+$lang['PH_AutoPrune_true'] = 'Разрешить';
+$lang['PH_Dump_NOK'] = 'Резервная копия не создана!';
+$lang['PH_Dump_OK'] = 'Резервная копия создана успешно!';
+$lang['PH_No_Backup_File'] = 'Нет резервной копии лоя восстановления!';
+$lang['PH_Restoration_NOK'] = 'Ошибка восстановления!';
+$lang['PH_Restoration_OK'] = 'Восстановление успешно завершено!';
+$lang['PH_Restore_File'] = 'Восстановить историю';
+?>

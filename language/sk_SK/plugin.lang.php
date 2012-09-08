38f1c7fc4ed9af77163903c3529c2910bdfa5701
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-08 17:14:58 +0000

    [prune_history] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@17822 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,61 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_Save_Title_d'] = 'Pretože je prečisťovanie nevratné, je dobré urobiť zálohu histórie pred samotným čistiacim úkonom. Vo východzom stave je tabuľka uložená ako súbor na servri a odtiaľ môže byť tiež obnovená.<br><br>
+Samozrejme je možné tento súbor uložiť na disk PC a tak urobiť zálohu mimo galérie. V takom prípade je obnovenie nutné urobiť cez váš nástroj pre prácu s databázou (napr. phpmyadmin, apod).';
+$lang['PH_ManualPrune_title_d'] = 'Táto funkcia umožňuje odstrániť záznamy v histórii na základ';
+$lang['PH_Support'] = 'Jediná oficiálna podpora tohto doplnku je v tomto vlákne na Piwigo fóre:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=19418" onclick="window.open(this.href);return false;">Anglické fórum - http://piwigo.org/forum/viewtopic.php?id=19418</a>';
+$lang['PH_Restore'] = 'Pre jednoduché obnovenie zo zálohy.<br>Je použitý len a len súbor, ktorý je uložený na servri.';
+$lang['PH_AutoRange_title_d'] = 'Prosím nastavte Históriu kritériám <b>uchovávania</b> pre Automatické čistenie. Viac možností je k dispozícii:<br><br>
+- Zachovať x dní<br>
+- Zachovať x mesiacov<br>
+- Zachovať x rokov<br><br>
+Napríklad: pokiaľ chcete zachovať históriu pre 3 mesiace, môžete nastaviť 3 mesiace alebo 91 dní.<br><br>
+Poznámka: Celková štatistika histórie nie je ovplyvnená Automatickým čistením.';
+$lang['PH_AutoPrune_title_d'] = 'Pokiaľ aktivujete Automatické čistenie, bude sa samo robiť pri každom prihlásení každého používateľa do galérie (vrátane prístupu administrátora alebo webmastra) a na základe zvoleného nastavenia.<br><br>
+<b style=&quot;color: red;&quot;>Používajte s rozumom! Takto odstránené dáta nie sú obnoviteľné!</b>';
+$lang['range']['3'] = 'Rok(y)';
+$lang['range']['2'] = 'Mesiac(e)';
+$lang['range']['1'] = 'Deň(dni)';
+$lang['Start_Date'] = 'Počiatočný dátum';
+$lang['PH_submit'] = 'Uložiť nastavenie';
+$lang['PH_save_config'] = 'Nastavenie uložené!';
+$lang['PH_Save_Title'] = 'Bezpečná záloha';
+$lang['PH_Save'] = 'Záloha histórie';
+$lang['PH_Restore_File'] = 'Obnovenie histórie';
+$lang['PH_Restoration_OK'] = 'Obnova bola dokončená!';
+$lang['PH_Restoration_NOK'] = 'Obnova nebola dokončená!';
+$lang['PH_Prune_Error'] = 'Chyba manuálneho čistenia! Skontrolujte rozsah pre dátumy.';
+$lang['PH_Prune_Done'] = 'Manuálne čistenie dokončené!';
+$lang['PH_Prune'] = 'Spustiť manuálne čistenie';
+$lang['PH_No_Backup_File'] = 'Súbor so zálohou nebol nájdený!';
+$lang['PH_ManualPrune_title'] = 'Manuálne prečistenie';
+$lang['PH_Dump_OK'] = 'Záloha bola dokončená!';
+$lang['PH_Dump_NOK'] = 'Záloha nebola dokončená!';
+$lang['PH_Dump_Download'] = 'Pre stiahnutie a uloženie súboru mimo server zvoľte tento bod:';
+$lang['PH_Bad_version_backup'] = 'Nesprávna verzia doplnku! Záloha, ktorú sa snažíte obnoviť, je zo staršej verzie, alebo bola vytvorená starším doplnkom.';
+$lang['PH_AutoRange_title'] = 'zachovať históriu až do:';
+$lang['PH_AutoPrune_true'] = 'Zapnuté';
+$lang['PH_AutoPrune_title'] = 'Automatické čistenie';
+$lang['PH_AutoPrune_false'] = 'Vypnuté (východzie)';
+?>

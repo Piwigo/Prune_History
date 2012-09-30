19024092e62853ac043d6d7c6668e251f8955197
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-30 18:53:49 +0000

    [prune_history] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@18387 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_Bad_version_backup'] = 'Versión incorrecta del plugin! La copia de seguridad que está intentando restaurar esta obsoleta o se creó con una versión anterior del complemento.';
+$lang['PH_AutoRange_title_d'] = 'Configure los criterios aquí <b> conservación </ b> historial de la poda automática. Varias opciones son posibles: <br>
+- Mantener x días<br>
+- Mantener x mes<br> 
+- Mantener x años<br>
+Por ejemplo: Si desea conservar una profundidad historial de 3 meses, puede configurar tres meses o 91 días<br><br>.
+Nota: el historial no se ve afectado por la poda automática.';
+$lang['PH_AutoPrune_title'] = 'Poda automatica';
+$lang['PH_AutoRange_title'] = 'seguir el historial hasta:';
+$lang['PH_AutoPrune_true'] = 'Permitir';
+$lang['PH_AutoPrune_title_d'] = 'Si habilita la poda automática, se activará automáticamente en cada inicio de sesión de usuario en la galería (incluyendo un administrador o webmaster) y en base a la configuración de conservación. <br><br>
+<b style=&quot;color: red;&quot;>
+Usar con precaución! Datos podados no será recuperable </ b>';
+$lang['PH_AutoPrune_false'] = 'Desactivado (por defecto)';
+?>

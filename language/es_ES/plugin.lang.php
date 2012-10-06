ebe324e99aec8d917944806da8e3c27f9f0faf96
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-06 08:18:54 +0000

    [prune_history] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@18503 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -34,4 +34,31 @@ $lang['PH_AutoPrune_title_d'] = 'Si habilita la poda automática, se activará a
 <b style=&quot;color: red;&quot;>
 Usar con precaución! Datos podados no será recuperable </ b>';
 $lang['PH_AutoPrune_false'] = 'Desactivado (por defecto)';
+$lang['PH_save_config'] = 'Configuración guardada!';
+$lang['PH_submit'] = 'Guarde la configuración';
+$lang['Start_Date'] = 'Fecha de inicio';
+$lang['range']['1'] = 'Días';
+$lang['range']['2'] = 'Mes';
+$lang['range']['3'] = 'Año(s)';
+$lang['PH_Support'] = 'Soporte oficial de este plugin es sólo se encuentra en este hilo del foro FR Piwigo <br>
+<a href="http://fr.piwigo.org/forum/viewtopic.php?id=22039" onclick="window.open(this.href);return false;">Forum français - http://fr.piwigo.org/forum/viewtopic.php?id=22039</a>';
+$lang['PH_Save_Title_d'] = 'La poda es definitiva e irreversible, es recomendable guardar la tabla de historial antes de empezar a podar. Por defecto, la tabla se almacena en un archivo en el servidor y se puede restaurar desde esta ubicación.<br><br>
+Pero también es posible descargar el archivo de copia de seguridad para mantenerlo fuera de la galería. En este caso, la restauración debe hacerse utilizando la base de datos de gestión de herramientas (normalmente phpMyAdmin).';
+$lang['PH_Dump_Download'] = 'Para descargar el archivo de copia de seguridad, marque esta casilla:';
+$lang['PH_Dump_NOK'] = 'La copia de seguridad falló!';
+$lang['PH_Dump_OK'] = 'La copia de seguridad se ha realizado correctamente!';
+$lang['PH_ManualPrune_title'] = 'Poda Manual';
+$lang['PH_No_Backup_File'] = 'No existe el archivo de copia de seguridad para restaurar!';
+$lang['PH_Prune'] = 'Realizar la poda manual';
+$lang['PH_Prune_Done'] = 'Poda manual hecho!';
+$lang['PH_Prune_Error'] = 'Error poda manual ! Revise su rango de fechas.';
+$lang['PH_Restoration_NOK'] = 'Restauración fallida!';
+$lang['PH_Restoration_OK'] = 'Restauración con éxito!';
+$lang['PH_Restore'] = 'Para restaurar una copia de seguridad de la tabla de la historia. <br> Sólo el archivo guardado en el servidor se toma en cuenta.';
+$lang['PH_Restore_File'] = 'Restaurar el histórial';
+$lang['PH_Save'] = 'Guardar historial';
+$lang['PH_Save_Title'] = 'Copia de seguridad';
+$lang['PH_ManualPrune_title_d'] = 'Esta función le permite borrar las entradas del historial basado en un rango de fechas. Seleccione una fecha de inicio y una fecha de finalización del tratamiento y correr.<br><br>
+<b style=&quot;color: red;&quot;>Advertencia: Los datos del historial se eliminarán de forma permanente </b> Puede guardar y restaurar la tabla del historial previo utilizando las funciones en el marco de la derecha.<br><br>
+Nota: El resumen del historial no se ve afectado por la poda.';
 ?>

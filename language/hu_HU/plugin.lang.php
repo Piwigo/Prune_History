e480577327e502a10277b289660629ef5afe9385
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-25 21:44:36 +0000

    [prune_history] Add hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@20402 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_Bad_version_backup'] = 'Hibás bővítmény verzió! A mentés elavult, vagy a bővítmény egy régebbi verziójával hozták létre.';
+$lang['PH_Save_Title'] = 'Biztonsági mentés';
+$lang['range']['3'] = 'Év(ek)';
+$lang['PH_save_config'] = 'Beállítások mentve!';
+$lang['PH_submit'] = 'Beállítások mentése';
+$lang['Start_Date'] = 'Induló dátum';
+$lang['range']['1'] = 'Nap(ok)';
+$lang['range']['2'] = 'Hónap(ok)';
+$lang['PH_AutoPrune_false'] = 'Kikapcsolva (alapértelmezett)';
+$lang['PH_AutoPrune_true'] = 'Bekapcsolva';
+$lang['PH_Dump_Download'] = 'Biztonsági mentés letöltése:';
+$lang['PH_Dump_NOK'] = 'A biztonsági mentés sikertelen!';
+$lang['PH_Dump_OK'] = 'A biztonsági mentés sikeres!';
+$lang['PH_No_Backup_File'] = 'A visszaállításhoz nem található biztonsági mentés!';
+$lang['PH_Restoration_NOK'] = 'Helyreállítás sikertelen!';
+$lang['PH_Restoration_OK'] = 'Helyreállítás sikeres!';
+$lang['PH_Restore_File'] = 'Előzmények visszaállítása';
+?>

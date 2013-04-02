2e871205394bab0d60113e91223b7ad88f83ae62
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-02 17:38:44 +0000

    [prune_history] Add pt_BR, thanks to : hespanhol
    git-svn-id: http://piwigo.org/svn/extensions/Prune_History@21978 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PH_Dump_Download'] = 'Para fazer o download da copia de segurança marque esta caixa:';
+$lang['PH_Save_Title'] = 'Copia de Segurança';
+$lang['PH_Save'] = 'Backup do histórico';
+$lang['PH_save_config'] = 'Configurações salvas!';
+$lang['PH_submit'] = 'Salvar configurações';
+$lang['Start_Date'] = 'Data inicial';
+$lang['range']['1'] = 'Dia(s)';
+$lang['range']['2'] = 'Mês(es)';
+$lang['range']['3'] = 'Ano(s)';
+$lang['PH_AutoPrune_false'] = 'Desabilitar (padrão)';
+$lang['PH_AutoPrune_title'] = 'Expurgo automático';
+$lang['PH_AutoPrune_true'] = 'Habilitar';
+$lang['PH_AutoRange_title'] = 'Manter o histórico até: ';
+$lang['PH_Dump_NOK'] = 'A copia de segurança falhou!';
+$lang['PH_Dump_OK'] = 'A copia de segurança foi feita com sucesso!';
+$lang['PH_ManualPrune_title'] = 'Expurgo manual';
+$lang['PH_No_Backup_File'] = 'Nenhum arquivo de backup foi encontrado para restaurar!';
+$lang['PH_Prune'] = 'Executar expurgo manual';
+$lang['PH_Prune_Done'] = 'Expurgo manual feito!';
+$lang['PH_Prune_Error'] = 'Erro no expurgo manual! Verifique o intervalo das datas.';
+$lang['PH_Restoration_NOK'] = 'Restauração falhou!';
+$lang['PH_Restoration_OK'] = 'Restaurado com sucesso!';
+$lang['PH_Restore_File'] = 'Restaurar histórico';
+?>

Piwigo/Prune_History

commit d8ceb81e49253be999330f90a0c870667f85e094

"summarized" column in History table is no longer used since Piwigo 2.9. Removed "summarized" from sql requests.

include/functions.inc.php
@@ -182,8 +182,7 @@ function history_prune($startdate,$stopdate)
 DELETE
 FROM '.HISTORY_TABLE.' 
 WHERE date < "'.$stopdate.'" 
-AND date > "'.$startdate.'"
-AND summarized = "true";';
+AND date > "'.$startdate.';';
 
     $r = pwg_query($query);
 
@@ -198,8 +197,7 @@ AND summarized = "true";';
   {
     $query = '
 DELETE FROM '.HISTORY_TABLE.' 
-WHERE date = "'.$stopdate.'" 
-AND summarized = "true";';
+WHERE date = "'.$stopdate.'"';
 
     $r = pwg_query($query);
 
@@ -247,8 +245,7 @@ function history_autoprune()
 
   $query = '
 DELETE FROM '.HISTORY_TABLE.' 
-WHERE date < "'.$limit.'"
-AND summarized = "true";';
+WHERE date < "'.$limit.'"';
 
   $r = pwg_query($query);
 

commit 4320e2b6d671cf4d08f13458e6f1df2c4cc2f050

Fix manual prune date range query
Executing a manual prune with date range (*not* the same day) resulted
in an SQL query error with
AND date > "2021-01-13;
that should be
AND date > "2021-01-13"

Regression from

    commit d8ceb81e49253be999330f90a0c870667f85e094
    CommitDate: Fri Mar 16 14:15:45 2018 +0100

        "summarized" column in History table is no longer used since Piwigo
        2.9. Removed "summarized" from sql requests.

include/functions.inc.php
@@ -182,7 +182,7 @@ function history_prune($startdate,$stopdate)
 DELETE
 FROM '.HISTORY_TABLE.' 
 WHERE date < "'.$stopdate.'" 
-AND date > "'.$startdate.';';
+AND date > "'.$startdate.'"';
 
     $r = pwg_query($query);
 
e728a09a1d74c9d03028c87705f8027b04ab1cd5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-17 14:16:53 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['PH_save_config'] = 'Nastavitve shranjene!';
+$lang['Start_Date'] = 'Začetni datum';
+$lang['PH_submit'] = 'Shrani nastavitve';
+$lang['PH_Save_Title_d'] = 'Ker je izločevanje dokončno in nepovratno, je priporočljivo, da pred začetkom izločevanja shranite svojo tabelo zgodovine. Tabela je privzeto shranjena v datoteko na strežniku in jo je mogoče od tam obnoviti.<br><br>
+Možno pa je tudi prenesti varnostno kopijo, da ne pride v galerijo. V tem primeru je treba obnovitev opraviti z orodjem za upravljanje baze podatkov (običajno phpMyAdmin).';
+$lang['PH_Restore'] = 'Za obnovitev varnostne kopije tabele zgodovine.<br>Upošteva se samo shranjena datoteka na strežniku.';
+$lang['PH_Restore_File'] = 'Obnovi zgodovino';
+$lang['PH_Save'] = 'Zgodovina varnostnega kopiranja';
+$lang['PH_Save_Title'] = 'Varnostna kopija';
+$lang['PH_Support'] = 'Uradna podpora za ta vtičnik je samo na tem forumu Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=19418" onclick="window.open(this.href);return false;">Angleški forum - http://piwigo.org /forum/viewtopic.php?id=19418</a>';
+$lang['range']['3'] = 'Leto(a)';
+$lang['range']['1'] = 'Dan(i)';
+$lang['range']['2'] = 'Mesec(i)';
+$lang['PH_Restoration_NOK'] = 'Obnova ni uspela!';
+$lang['PH_Restoration_OK'] = 'Obnova uspela!';
+$lang['PH_Prune_Error'] = 'Napaka pri ročnem izločanju! Preverite svoje časovno obdobje.';
+$lang['PH_Prune'] = 'Izvedite ročno izločanje';
+$lang['PH_Prune_Done'] = 'Ročno izločanje opravljeno!';
+$lang['PH_Dump_Download'] = 'Če želite prenesti varnostno kopijo datoteke, potrdite to polje:';
+$lang['PH_Dump_NOK'] = 'Varnostno kopiranje ni uspelo!';
+$lang['PH_Dump_OK'] = 'Varnostno kopiranje je bilo uspešno!';
+$lang['PH_No_Backup_File'] = 'Varnostne kopije ni bilo mogoče obnoviti!';
+$lang['PH_Bad_version_backup'] = 'Napačna različica vtičnika! Varnostna kopija, ki jo poskušate obnoviti, je zastarela ali je bila ustvarjena s starejšo različico vtičnika.';
+$lang['PH_AutoRange_title'] = 'Ohrani zgodovino do:';
+$lang['PH_AutoPrune_false'] = 'Onemogoči (privzeto)';
+$lang['PH_AutoPrune_true'] = 'Omogoči';
+$lang['PH_AutoPrune_title'] = 'Samodejno izločanje';
+$lang['PH_AutoPrune_title_d'] = 'Če omogočite samodejno izločanje, se bo samodejno sprožilo ob vsaki prijavi uporabnika v galerijo (vključno s skrbnikom ali spletnim skrbnikom) in na podlagi nastavitev ohranjanja.<br><br>
+<b style=&quot;color: red;&quot;>Uporabljajte previdno! Izločenih podatkov ne bo mogoče obnoviti!</b>';
+$lang['PH_ManualPrune_title'] = 'Ročno izločiti';
+$lang['PH_ManualPrune_title_d'] = 'Ta funkcija vam omogoča, da izbrišete vnose zgodovine glede na vrsto datumov. Izberite začetni in končni datum zdravljenja in zaženite.<br><br>
+<b style=&quot;color: red;&quot;>Opozorilo: podatki o zgodovini bodo nepovratno izbrisani!</b> Tabelo zgodovine lahko varnostno kopirate in obnovite s funkcijami v desnem okviru.<br><br>
+Opomba: izločevanje ne vpliva na povzetek zgodovine.';
+$lang['PH_AutoRange_title_d'] = 'Prosimo, konfigurirajte merilo <b>ohranjevanja</b> zgodovine za samodejno izločanje. Možnih je več izbir:<br><br>
+- Obdrži x dni<br>
+- Obdrži x mesecev<br>
+- Obdrži x let<br><br>
+Na primer: če želite ohraniti zgodovinsko globino 3 mesece, lahko nastavite tri mesece ali 91 dni.<br><br>
+Opomba: samodejno izločanje ne vpliva na povzetek zgodovine.';

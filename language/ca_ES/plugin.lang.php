a1019f186e0af89cc4482783c19e0169d0b79947
ddtddt <ddtddt@piwigo.org>
2022-01-17 14:10:57 +0100

    Add CA_es recup SVN

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['range']['3'] = 'Any(s)';
+$lang['PH_ManualPrune_title_d'] = 'Aquesta característica us permet eliminar entrades de l\'historial segons un interval de dates. Trieu una data d\'inici i la data de finalització del tractament, i executeu-ho.<br><br>
+<b style=&quot;color: red;&quot;>Avís: Les dades de l\'historial se suprimiran irremeiablement!</b> Podeu fer una còpia de seguretat i restaurar la taula de l\'historial utilitzant les funcions del marc correcte.<br><br>
+Nota: El resum de l\'historial no es veu afectat per la poda.';
+$lang['PH_Support'] = 'El suport oficial d\'aquest connector és només en aquest tema del fòrum Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=19418" onclick="window.open(this.href);return false;">fòrumen anglès - http://piwigo.org/forum/viewtopic.php?id=19418</a>';
+$lang['PH_Restoration_OK'] = 'Restauració acabada!';
+$lang['PH_Restore'] = 'Per restaurar una còpia de seguretat de la taula de l\'historial. <br>Només es té en compte el fitxer desat al servidor.';
+$lang['PH_Restore_File'] = 'Restaura l\'historial';
+$lang['PH_Save'] = 'Historial de còpies de seguretat';
+$lang['PH_Save_Title'] = 'Còpia de seguretat';
+$lang['PH_Save_Title_d'] = 'Com que la poda és definitiva i irreversible, és aconsellable guardar la vostra taula d\'història abans de començar a podar. Per defecte, la taula es desa en un fitxer del servidor i es pot restaurar des d\'allà.<br><br>
+Però també és possible descarregar el fitxer de còpia de seguretat per mantenir-lo fora de la vostra galeria. En aquest cas, la restauració s\'ha de fer a través de la vostra eina de gestió de bases de dades (normalment phpMyAdmin).';
+$lang['PH_save_config'] = 'Configuració desada!';
+$lang['PH_submit'] = 'Desa la configuració';
+$lang['Start_Date'] = 'Data d\'inici';
+$lang['range']['1'] = 'Dia(es)';
+$lang['range']['2'] = 'Mes(s)';
+$lang['PH_Dump_NOK'] = 'La còpia de seguretat ha fallat!';
+$lang['PH_Dump_OK'] = 'La còpia de seguretat ha estat correcta!';
+$lang['PH_ManualPrune_title'] = 'Manual Prune';
+$lang['PH_No_Backup_File'] = 'No s\'ha trobat cap fitxer de còpia de seguretat per restaurar!';
+$lang['PH_Prune'] = 'Executa la poda manual';
+$lang['PH_Prune_Done'] = 'S\'ha acabat la poda manual!';
+$lang['PH_Prune_Error'] = 'Error en podar manualment! Comproveu l\'interval de dates.';
+$lang['PH_Restoration_NOK'] = 'La restauració ha fallat!';
+$lang['PH_AutoPrune_false'] = 'Desactiva (per defecte)';
+$lang['PH_AutoPrune_title'] = 'Prune automàtic';
+$lang['PH_AutoPrune_title_d'] = 'Si habiliteu la poda automàtica, s\'activarà automàticament a cada usuari que iniciï la sessió a la galeria (incloent-hi un administrador o un administrador web) i basant-se en la configuració de conservació.
+<br><br>
+<b style=&quot;color: red;&quot;>Utilitza amb precaució! No es podran recuperar les dades proporcionades!</b>';
+$lang['PH_AutoPrune_true'] = 'Activa';
+$lang['PH_AutoRange_title'] = 'manté l\'historial fins a:';
+$lang['PH_AutoRange_title_d'] = 'Si us plau, configureu el vostre criteri de <b>conservació</b> de l\'historial per a la poda automàtica. Hi ha diverses opcions: <br><br>
+- Mantingui x dies<br>
+- Mantenir x mesos<br>
+- Mantenir x anys<br><br>
+Per exemple: si voleu mantenir una profunditat històrica de 3 mesos, podeu configurar tres mesos o 91 dies. <br><br>
+Nota: El resum de l\'historial no es veu afectat per la poda automàtica.';
+$lang['PH_Bad_version_backup'] = 'Versió del connector incorrecta! La còpia de seguretat que esteu intentant restaurar està obsoleta o s\'ha creat amb una versió més antiga del connector.';
+$lang['PH_Dump_Download'] = 'Per baixar el fitxer de còpia de seguretat, marqueu aquesta casella:';
--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -20,25 +20,25 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['PH_ManualPrune_title_d'] = 'Met deze functie kunt u de geschiedenis verwijderen op basis van een data-range. Kies een begindatum en einddatum van de behandeling en voer de plugin uit.<br><br>
-<b style=&quot;color:red;&quot;> Waarschuwing: De geschiedenis gegevens zullen onherroepelijk worden verwijderd </ b> U kunt back-up en herstel van de geschiedenis tafel met behulp van de functies in de juiste frame <br>!.
-Opmerking: De geschiedenis samenvatting wordt niet beïnvloed door snoeien.';
-$lang['PH_AutoPrune_title_d'] = 'Indien u automatische schonen inschakelt, wordt deze automatisch geactiveerd bij elke aanmelding van een gebruiker (inclusief beheerder of webmaster) en gebaseerd op de aangegeven instellingen.<br>
+$lang['PH_ManualPrune_title_d'] = 'Met deze functie kunt je de geschiedenis verwijderen op basis van een datumbereik. Kies een begindatum en einddatum van de behandeling en voer de plugin uit.<br><br>
+<b style=&quot;color:red;&quot;> Waarschuwing: De geschiedenis- gegevens zullen onherroepelijk worden verwijderd </ b> Je kunt back-up en herstel van de geschiedenis tabel met behulp van de functies in de juiste frame <br>!.
+Opmerking: De geschiedenis-samenvatting wordt niet beïnvloed door snoeien.';
+$lang['PH_AutoPrune_title_d'] = 'Als je automatische schonen inschakelt, wordt deze automatisch geactiveerd bij elke aanmelding van een gebruiker (inclusief beheerder of webmaster) en gebaseerd op de aangegeven instellingen.<br>
 <b style=&quot;color: red;&quot;> Wees voorzichtig! Geschoonde gegevens kunnen niet meer hersteld worden! </ B>';
-$lang['PH_AutoRange_title_d'] = 'Configureer uw Geschiedenis<b>bewaar</b>criterium voor automatisch schonen. Verschillende keuzes zijn mogelijk: <br><br>
+$lang['PH_AutoRange_title_d'] = 'Configureer jouw Geschiedenis<b>bewaar</b>criterium voor automatisch schonen. Verschillende keuzes zijn mogelijk: <br><br>
 - Bewaar x dagen <br>
 - Bewaar x maanden <br>
 - Bewaar x jaar <br><br>
-Bijvoorbeeld: Als u 3 maanden geschiedenis wilt bewaren, geeft u op: drie maanden of 91 dagen <br><br>.
+Bijvoorbeeld: Als je 3 maanden geschiedenis wilt bewaren, geef je op: drie maanden of 91 dagen <br><br>.
 NB: De Geschiedenissamenvatting wordt niet beïnvloed door automatisch schonen.';
-$lang['PH_Save_Title_d'] = 'Omdat schonen definitief en onomkeerbaar is, wordt u aangeraden om uw geschiedenistabel op te slaan voordat u begint met schonen. Standaard wordt de tabel opgeslagen in een bestand op de server en kan van diezelfde server worden hersteld.<br><br>
-Maar het is ook mogelijk om het backupbestand te downloaden om het uit je galerij te houden. In dat geval dient het herstel via uw database management tool (meestal phpMyAdmin) uitgevoerd worden.';
-$lang['PH_Support'] = 'De officiële support voor deze plugin kunt u vinden op het Piwigo-forum via de volgende link:<br>
+$lang['PH_Save_Title_d'] = 'Omdat schonen definitief en onomkeerbaar is, word je aangeraden om jouw geschiedenistabel op te slaan voordat u begint met schonen. Standaard wordt de tabel opgeslagen in een bestand op de server en kan van diezelfde server worden hersteld.<br><br>
+Maar het is ook mogelijk om het backupbestand te downloaden om het buiten jouw galerij te houden. In dat geval dient het herstel via je database management tool (meestal phpMyAdmin) uitgevoerd worden.';
+$lang['PH_Support'] = 'De officiële support voor deze plugin kun je vinden op het Piwigo-forum via de volgende link:<br>
 <a href="http://piwigo.org/forum/viewtopic.php?id=19418" onclick="window.open(this.href);return false;">Engels forum - http://piwigo.org/forum/viewtopic.php?id=19418</a>';
 $lang['PH_Save'] = 'Backup geschiedenis';
 $lang['PH_Save_Title'] = 'Veiligheidsbackup';
-$lang['PH_save_config'] = 'Instellingen opgeslagen!';
-$lang['PH_submit'] = 'Sla instellingen op';
+$lang['PH_save_config'] = 'Instellingen bewaard!';
+$lang['PH_submit'] = 'Bewaar instellingen';
 $lang['Start_Date'] = 'Startdatum';
 $lang['range']['1'] = 'Dag(en)';
 $lang['range']['2'] = 'Maand(en)';
@@ -46,18 +46,17 @@ $lang['range']['3'] = 'Ja(a)r(en)';
 $lang['PH_AutoPrune_title'] = 'Automatische schoning';
 $lang['PH_AutoPrune_true'] = 'Aanzetten';
 $lang['PH_AutoRange_title'] = 'De geschiedenis bewaren tot:';
-$lang['PH_Bad_version_backup'] = 'Verkeerde plugin versie! De backup die u probeert terug te zetten is verouderd of is gemaakt met een oudere versie van de plugin.';
-$lang['PH_Dump_Download'] = 'Om de backup terug te zetten, vink dit vakje:';
+$lang['PH_Bad_version_backup'] = 'Verkeerde plugin-versie! De backup die je probeert terug te zetten is verouderd of is gemaakt met een oudere versie van de plugin.';
+$lang['PH_Dump_Download'] = 'Vink dit vakje om de backup terug te zetten:';
 $lang['PH_Dump_NOK'] = 'Backup mislukt!';
 $lang['PH_Dump_OK'] = 'De backup is gelukt.';
 $lang['PH_ManualPrune_title'] = 'Handmatige schoning';
-$lang['PH_No_Backup_File'] = 'Geen backupo gevonden die teruggezet kan worden!';
+$lang['PH_No_Backup_File'] = 'Geen backup gevonden die teruggezet kan worden!';
 $lang['PH_Prune'] = 'Voer een handmatige schoning uit!';
 $lang['PH_Prune_Done'] = 'Handmatige schoning uitgevoerd!';
-$lang['PH_Prune_Error'] = 'Fout tijdens handmatige schoning. Controleer uw datumreeks.';
+$lang['PH_Prune_Error'] = 'Fout tijdens handmatige schoning. Controleer jouw datumbereik.';
 $lang['PH_Restoration_NOK'] = 'Terugzetten mislukt!';
 $lang['PH_Restoration_OK'] = 'Terugzetten gelukt!';
-$lang['PH_Restore'] = 'Om een backup van uw geschiedenistabel terug te zetten.<br> Alleen het op de server opgeslagen bestand wordt in beschouwing genomen.';
+$lang['PH_Restore'] = 'Om een backup van jouw geschiedenistabel terug te zetten.<br> Alleen het op de server opgeslagen bestand wordt in beschouwing genomen.';
 $lang['PH_Restore_File'] = 'Geschiedenis terugzetten';
 $lang['PH_AutoPrune_false'] = 'Uitzetten (standaard)';
-?>

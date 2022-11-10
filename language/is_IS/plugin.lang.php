0b1a1f1b9b061540fc9e98f2ac96a45a300726ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-10 13:56:35 +0100

    Update is_IS, thanks to gudjonsi

--- language/is_IS/plugin.lang.php
+++ language/is_IS/plugin.lang.php
@@ -6,3 +6,42 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['PH_AutoPrune_false'] = 'Afvirkja (sjálfgefið)';
+$lang['PH_Save_Title'] = 'Öryggisafrit';
+$lang['PH_Save_Title_d'] = 'Vegna þess að hreinsun er endanleg og óafturkræf er ráðlegt að vista sögutöfluna þína áður en byrjað er að hreinsa. Sjálfgefið er að taflan er vistuð í skrá á vefþjóninum og hægt er að endurheimta hana þaðan.<br><br>
+En það er líka hægt að hlaða niður öryggisafritinu til að halda henni utan við myndasafnið þitt. Í þessu tilviki verður endurheimtin að fara fram í gegnum gagnagrunnsstjórnunartólið þitt (venjulega phpMyAdmin).';
+$lang['PH_Support'] = 'Opinber stuðningur við þessa viðbót er aðeins í þessu Piwigo umræðuefni:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=19418" onclick="window.open(this.href);return false;">Enskt spjallborð - http://piwigo.org /forum/viewtopic.php?id=19418</a>';
+$lang['PH_save_config'] = 'Stillingar vistaðar!';
+$lang['PH_submit'] = 'Vista stillingar';
+$lang['Start_Date'] = 'Upphafsdagur';
+$lang['range']['1'] = 'Dag(-a)';
+$lang['range']['2'] = 'Mánuð(-i)';
+$lang['range']['3'] = 'Ár';
+$lang['PH_AutoPrune_title'] = 'Sjálfvirk hreinsun';
+$lang['PH_AutoPrune_title_d'] = 'Ef þú virkjar sjálfvirka hreinsun þá verður hún virkjuð sjálfkrafa fyrir hverja notandainnskráningu í myndasafnið (þar á meðal hjá stjórnanda eða vefstjóra) og byggt á varðveislustillingunum.<br><br>
+<b style=&quot;color: red;&quot;>Notið með varúð! Ekki er hægt að endurheimta hreinsuð gögn!</b>';
+$lang['PH_AutoPrune_true'] = 'Virkja';
+$lang['PH_AutoRange_title'] = 'haltu í söguna upp að:';
+$lang['PH_AutoRange_title_d'] = 'Vinsamlega stilltu <b>varðveislu</b> viðmið sögunnar fyrir sjálfvirka hreinsun. Nokkrir valkostir eru mögulegir:<br><br>
+- Geymdu x daga<br>
+- Geymdu x mánuði<br>
+- Geymdu x ár<br><br>
+Til dæmis: Ef þú vilt geyma sögulega dýpt upp á 3 mánuði þá geturðu sett upp þrjá mánuði eða 91 dag.<br><br>
+Athugið: Söguyfirlitið verður ekki fyrir áhrifum af hreinsun.';
+$lang['PH_Bad_version_backup'] = 'Röng útgáfa viðbótar! Afritið sem þú ert að reyna að endurheimta er úrelt eða hefur verið búið til með eldri útgáfu af viðbótinni.';
+$lang['PH_Dump_Download'] = 'Til að hlaða niður öryggisafritinu skaltu haka við þennan reit:';
+$lang['PH_Dump_NOK'] = 'Afritunin mistókst!';
+$lang['PH_Dump_OK'] = 'Afritunin tókst!';
+$lang['PH_ManualPrune_title'] = 'Handvirk hreinsun';
+$lang['PH_ManualPrune_title_d'] = 'Þessi eiginleiki gerir þér kleift að eyða sögufærslum á grundvelli dagsetningatímabils. Veldu upphafsdag og lokadag meðferðar og settu af stað.<br><br>
+<b style=&quot;color: red;&quot;>Viðvörun: Sögugögnum verður eytt með óafturkræfum hætti!</b> Þú getur tekið öryggisafrit og endurheimt sögutöflu með því að nota aðgerðirnar í hægri rammanum.<br><br>
+Athugið: Söguyfirlitið verður ekki fyrir áhrifum af hreinsun.';
+$lang['PH_No_Backup_File'] = 'Ekkert öryggisafrit fannst til að endurheimta!';
+$lang['PH_Prune'] = 'Framkvæma handvirka hreinsun';
+$lang['PH_Prune_Done'] = 'Handvirk hreinsun framkvæmd!';
+$lang['PH_Prune_Error'] = 'Villa við handvirka hreinsun! Athugaðu dagsetningartímabilið þitt.';
+$lang['PH_Restoration_NOK'] = 'Endurheimt mistókst!';
+$lang['PH_Restoration_OK'] = 'Endurheimt tókst!';
+$lang['PH_Restore'] = 'Til að endurheimta öryggisafrit af sögutöflunni þinni.<br>Aðeins vistuð skrá á vefþjóninum er tekin með í reikninginn.';
+$lang['PH_Restore_File'] = 'Endurheimta sögu';
+$lang['PH_Save'] = 'Afritunarsaga';
